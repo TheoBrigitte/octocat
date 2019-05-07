b919bc69226c1ee07d1717c2277f0c9268302328
Theo Brigitte <theo.brigitte@gmail.com>
2019-05-07 19:58:14 +0200

    define expected project

--- pkg/project/project_test.go
+++ pkg/project/project_test.go
@@ -4,7 +4,45 @@ import (
 	"net/url"
 	"os"
 	"testing"
+	"time"
+
+	"github.com/TheoBrigitte/octocat/pkg/project"
 )
+
+var p project.Project
+
+func init() {
+	u, err := url.Parse("")
+	if err != nil {
+		panic(err)
+	}
+
+	p = project.Project{
+		Title:        "",
+		Description:  "",
+		Localisation: "",
+		Theme:        "",
+		Like:         0,
+		Follower:     0,
+		IsPopular:    false,
+		Year:         0,
+		Status:       "",
+		Cost:         0,
+		Author:       "",
+		URL:          u,
+		Creation: project.Creation{
+			Author: "",
+			Date:   time.Date(2019, 12, 31, 12, 0, 0, 0, time.UTC),
+		},
+		Comment: []project.Comment{
+			{
+				Author: "",
+				Date:   time.Date(2019, 12, 31, 12, 0, 0, 0, time.UTC),
+				Text:   "",
+			},
+		},
+	}
+}
 
 func TestParse(t *testing.T) {
 	file, err := os.Open("../../fixtures/project.html")
